97fe1894c8d64aac92a38995dc3eaed26c5a488f
Franco Olmedo <franco.olm95@gmail.com>
2023-01-20 21:28:00 -0300

    Guardo modificaciones

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,19 @@
     document.getElementById("boton_fecha").onmouseover = function fecha() {
         document.getElementById("fecha").innerHTML = Date();
     }
+
+    // Modo Oscuro
+    document.getElementById("dark_mode").addEventListener("click", function darkMode() {
+        document.body.style.backgroundColor = "#212F3D";
+        document.getElementById("content").style.backgroundColor = "#566573";
+        document.getElementById("sidebar").style.backgroundColor = "#2C3E50";
+        
+        const collection = document.getElementsByTagName("ul");
+        for (let i = 0; i < collection.length; i++) {
+            collection[i].style.backgroundColor = "#808B96";
+        }
+
+        const parrafo = document.getElementById("fecha");
+        parrafo.style.color = "white";
+        parrafo.style.fontWeight = "normal";
+    })
